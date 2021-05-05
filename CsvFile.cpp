c963fcb19654f1bfe20a4a3b832d9e21f0c29aaa
CPSLaboratories <przemkopp@gmail.com>
2021-05-05 20:47:24 +0200

    ABC

--- CsvFile.cpp
+++ CsvFile.cpp
@@ -5,21 +5,26 @@
 #include "FileError.h"
 #include <string>
 
+using namespace std;
+
 CsvFile::CsvFile(const std::string filePath, const std::string mode) : filePath(filePath), length(0)
 {
-	openMode = std::fstream::out;
+	//openMode = std::fstream::out;
 
 	if (mode.find("r") != std::string::npos) openMode |= std::fstream::in;
+	if (mode.find("w") != std::string::npos) openMode |= std::fstream::out;
 	if (mode.find("a") != std::string::npos) openMode |= std::fstream::app;
 	if (mode.find("t") != std::string::npos) openMode |= std::fstream::trunc;
 	if (mode.find("e") != std::string::npos) openMode |= std::fstream::ate;
 
 	file.open(filePath, openMode);
-	if (file.is_open()) {
-		file.seekg(0, std::fstream::end);
-		length = file.tellg();
-		file.seekg(0, std::fstream::beg);
-	}
+
+	//if (file.is_open()) 
+	//{
+	//	file.seekg(0, std::fstream::end);
+	//	length = file.tellg();
+	//	file.seekg(0, std::fstream::beg);
+	//}
 }
 
 CsvFile::~CsvFile()
@@ -29,6 +34,8 @@ CsvFile::~CsvFile()
 
 FileError CsvFile::Write(const std::vector<Point>& v)
 {
+	int Rozmiar = v.size();
+
 	FileError retVal = FileError(ACCESS_DENIED);
 
 	if (!file.is_open())
@@ -37,7 +44,15 @@ FileError CsvFile::Write(const std::vector<Point>& v)
 	}
 	else if (openMode & std::fstream::out)
 	{
-		file.write((const char*)v.data(), v.size() * sizeof(Point));
+		string Dane = "";
+
+		for(int i = 0; i < Rozmiar; i++)
+		{
+			Dane += to_string(v[i].x) + "," + to_string(v[i].y) + "," + to_string(v[i].z) + "\n";
+		}
+
+		file << Dane;
+
 		retVal = FileError(SUCCESS);
 	}
 
---
+++ main.cpp
@@ -0,0 +1,59 @@
+/*-----------------Programowanie Obiektowe/AiR/Semestr 2/Grupa 5/Sekcja 5.9------------------------------
+ ------------------Przemys³aw Papiernik/Jakub Wrzeszcz/Sebastian Likoñski-----------------------------*/
+
+#include "BinaryFile.h"
+#include "CsvFile.h"
+#include "Point.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	//char ktoryplik;
+	//char tryb;
+	
+	//cout << "Ktory plik chcesz otworzyc? Wybierz odpowiednia litere:" << endl;
+	//cout << "b - plik binarny" << endl;
+	//cout << "c - plik tekstowy o wartosciach rozdzielonych" << endl;
+
+	//cin >> ktoryplik;
+
+	//if (ktoryplik != 'b' && ktoryplik != 'c')
+	//{
+	//	cout << "Nie rozpoznano typu pliku" << endl;
+	//}
+
+	//cout << "Jak chcesz otworzyc plik? Wybierz odpowiednia litere:\n" << endl;
+	//cout << "r - odczytywanie danych" << endl;
+	//cout << "w - zapisywanie danych" << endl;
+	//cout << "a - zapisywanie danych na koncu pliku" << endl;
+	//cout << "t - kasowanie zawatosci" << endl;
+	//cout << "e - ustawienie wskaznika na koniec pliku\n" << endl;
+	
+	//cin >> tryb;
+
+	//if (ktoryplik == 'b')
+	//{
+	//	//otwarcie BinaryFile
+	//}
+
+	//else
+	//{
+		//otwarcie CsvFile
+		vector <Point> dane;
+		dane.push_back(Point(1,2,30));
+		dane.push_back(Point(1,2,31));
+		dane.push_back(Point(1,2,32));
+		dane.push_back(Point(1,2,33));
+		dane.push_back(Point(1,2,34));
+		CsvFile::CsvFile Plik;
+		Plik = CsvFile::CsvFile("test2.csv", "w");
+		Plik.Write(dane);
+
+	//}
+
+}
+
